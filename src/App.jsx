62bb99c8b526c5545b8408400cba02e576063adb
Eugene Afriyie <groupeight00@gmail.com>
2025-07-12 03:07:10 +0000

    HeroCarousel code

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
-import About from './Pages/About/About';
+import About from './Pages/About/Components/About';
 import Gallery from './Pages/Gallery/Gallery';
 import Contact from './Pages/Contact/Contact';
 import Services from './Pages/Home/Components/Services';
